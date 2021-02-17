e3a9349ebabab379077a65fa55ebcf80ebae84e9
walterje <walterje07@gmail.com>
2021-02-16 22:00:24 -0300

    Creacion de la calse Empleado

--- Aplicaciones/Ventas/models.py
+++ Aplicaciones/Ventas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Empleado(models.Model):
+    cod_empleado = models.CharField(primary_key=True, max_length=8, verbose_name='Código')
+    nombres = models.CharField(max_length=20, verbose_name='Nombres')
+    apellidos = models.CharField(max_length=20, verbose_name='Apellidos')
+
+def __str__(self):
+    return "{0} {1}".format(self.nombres, self.apellidos)
