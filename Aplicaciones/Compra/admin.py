bf19a84f688f318e639441e20138020410c9e0ee
Anto <anttosarubi@gmail.com>
2021-02-22 17:08:42 -0300

    Registrar tipo productos, productos, pedidos_compra

--- Aplicaciones/Compra/admin.py
+++ Aplicaciones/Compra/admin.py
@@ -5,7 +5,20 @@ class proveedorAdmin(admin.ModelAdmin):
     list_display = ("nombre", "cod_proveedor")
     search_fields = ["nombre", "cod_proveedor"]
 
+class tipoProductoAdmin(admin.ModelAdmin):
+    list_display=("cod_tipo_producto","descripcion")
+    search_fields=("cod_tipo_producto","descripcion")
 
+class productosAdmin(admin.ModelAdmin):
+    list_display=("cod_producto","nombre_producto")
+    search_fields=("cod_producto","nombre_producto")
+
+class pedidoCompraAdmin(admin.ModelAdmin):
+    list_display=("nro_pedido_compra","descripcion_pedidio")
+    search_fields=("nro_pedido_compra","descripcion_pedidio")
 # Register your models here.
 
 admin.site.register(Proveedor, proveedorAdmin)
+admin.site.register(Tipo_producto,tipoProductoAdmin)
+admin.site.register(Producto,productosAdmin)
+admin.site.register(Pedidos_Compra,pedidoCompraAdmin)
--- Aplicaciones/Compra/models.py
+++ Aplicaciones/Compra/models.py
@@ -21,3 +21,48 @@ class Proveedor(models.Model):
         verbose_name_plural = 'Proveedores'
         db_table = 'proveedor'
     
+class Tipo_producto(models.Model):
+    cod_tipo_producto=models.IntegerField(primary_key=True,verbose_name="Codigo Tipo Producto")
+    descripcion=models.CharField(max_length=30,verbose_name="Tipo Producto")
+
+    def __str__(self):
+        return "{0} {1}".format(self.cod_tipo,self.descripcion)
+
+    class Meta:
+        verbose_name= 'Tipo Producto'
+        db_table='Tipo_producto'
+
+class Producto(models.Model):
+    cod_producto = models.IntegerField(primary_key=True,verbose_name = "Codigo Producto")
+    nombre_producto = models.CharField(max_length=30,verbose_name="producto")
+    descripcion = models.CharField(max_length=30)
+    cod_tipo_producto = models.ForeignKey(Tipo_producto,blank=False,null=False,on_delete=models.PROTECT)
+    cant_stock = models.IntegerField()
+    cant_minima_stock = models.IntegerField()
+    estado=models.CharField(max_length=15)
+
+    def __str__(self):
+        return "{0} {1}".format(self.cod_producto,self.descripcion)
+
+    class Meta:
+        verbose_name ='Producto'
+        verbose_name_plural ='Productos'
+        db_table = 'Producto'
+
+class Pedidos_Compra(models.Model):
+   nro_pedido_compra = models.IntegerField(primary_key=True,verbose_name="Numero Pedido")
+   fecha_pedido_compra=models.DateField(auto_now=True)
+   cod_proveedor=models.ForeignKey(Proveedor,blank= False, null=False, on_delete=models.PROTECT) 
+   cod_producto=models.ForeignKey(Producto,blank= False, null=False, on_delete=models.PROTECT)
+   cantidad=models.IntegerField()
+   descripcion_pedidio=models.CharField(max_length=50,verbose_name="Pedido_compra")
+   
+   def __str__(self):
+        return "{0} {1}".format(self.nro_pedido_compra,self.descripcion_pedidio)
+    
+   class Meta:
+       verbose_name = 'Pedido_compra'
+       verbose_name_plural = 'Pedidos_compras'
+       db_table = 'Pedido_compra'
+
+
